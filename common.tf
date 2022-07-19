05d76069483a40ae0060448f4297ef1548431478
Phil Wilkins <phil@mp3monster.org>
2022-07-19 20:28:08 +0100

    push down the version and the tag namespace

--- common.tf
+++ common.tf
@@ -1,10 +1,6 @@
 ## Copyright (c) 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-#module "set_tags" {
-#  source        = "github.com/oracle-devrel/terraform-oci-arch-std-tags"
-#  tag_namespace = "terraform-oci-arch-oke-mds"
-#}
 locals {
   defined_tags = module.policies.predefined_tags
   # declare a local for defined tags to conceal from the rest of the module how we're creating them
@@ -15,4 +11,6 @@ module "policies" {
   tenancy_ocid                  = var.tenancy_ocid
   compartment_ocid              = var.compartment_ocid
   region_name                   = var.region
+  tag_namespace                 = "terraform-oci-arch-oke-mds"
+  release                       = var.release
 }
